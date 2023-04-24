9beeb1390316af583242fb95c52f6496eb761e8f
Yiorgis Gozadinos <ggozadinos@gmail.com>
2023-04-24 17:20:16 +0200

    NATSContext

--- frontend/components/NATSContext.tsx
+++ frontend/components/NATSContext.tsx
@@ -1,4 +1,12 @@
-import { connect } from 'nats.ws'
+import {
+  Codec,
+  NatsConnection,
+  PublishOptions,
+  StringCodec,
+  Subscription,
+  SubscriptionOptions,
+  connect,
+} from 'nats.ws'
 import {
   Context,
   ReactNode,
@@ -6,33 +14,62 @@ import {
   useCallback,
   useContext,
   useEffect,
-  useRef,
   useState,
 } from 'react'
 
 export interface NATSState {
-  connection: any
+  connection: NatsConnection | null
+  connect: () => Promise<void>
+  disconnect: () => Promise<void>
+  publish: (subject: string, msg?: Uint8Array, options?: PublishOptions) => void
+  subscribe: (subject: string, options?: SubscriptionOptions) => Subscription
+  codec: Codec<string>
 }
 
 const natsContext: Context<NATSState | undefined> = createContext(undefined)
 
+const codec = StringCodec()
+
 export function NATSProvider({ children }: { children: ReactNode }) {
-  const [connection, setConnection] = useState<any>(null)
-  useEffect(() => {
-    async function serverConnect() {
-      const nc = await connect({
-        servers: [process.env.NEXT_PUBLIC_NATS_WS],
-      })
+  const [connection, setConnection] = useState<NatsConnection | null>(null)
+
+  const serverDisconnect = useCallback(async () => {}, [])
+
+  const serverConnect = useCallback(async () => {
+    return await connect({
+      servers: [process.env.NEXT_PUBLIC_NATS_WS],
+    }).then((nc) => {
       setConnection(nc)
-      console.log('connected')
+    })
+  }, [])
+
+  const publish = useCallback(
+    (subject: string, msg?: Uint8Array, options?: PublishOptions) => {
+      connection.publish(subject, msg, options)
+    },
+    [connection]
+  )
+  const subscribe = useCallback(
+    (subject: string, options?: SubscriptionOptions) =>
+      connection.subscribe(subject, options),
+    [connection]
+  )
+
+  useEffect(() => {
+    if (!connection) {
+      serverConnect()
     }
-    if (!connection) serverConnect()
-  }, [connection])
+  }, [connection, serverConnect])
 
   return (
     <natsContext.Provider
       value={{
         connection,
+        connect: serverConnect,
+        disconnect: serverDisconnect,
+        publish,
+        subscribe,
+        codec,
       }}
     >
       {children}
