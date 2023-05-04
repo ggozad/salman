b7adf10a2c06ee790bc8a93fed3cd954063faaf1
Yiorgis Gozadinos <ggozadinos@gmail.com>
2023-05-04 18:28:36 +0200

    Make Recorder a context management and iterable. Integrate with textual app.

---
+++ salman/salman/app/app.py
@@ -0,0 +1,81 @@
+import asyncio
+import uuid
+
+from pydub import AudioSegment
+from textual import on
+from textual.app import App, ComposeResult
+from textual.containers import ScrollableContainer
+from textual.reactive import reactive
+from textual.widgets import Button, Footer, Header, Input, Static
+
+from salman.voice.recorder import MicRecorder
+from salman.workers.voice import end_recording, post_blob, start_recording
+
+
+class PromptWidget(Static):
+    id = "prompt"
+    uid = ""
+    is_recording = False
+    text = reactive("")
+
+    async def _record_and_transcribe(self):
+        count = 0
+        with MicRecorder() as recorder:
+            async for blob, _ in recorder:
+                print(count, self.is_recording)
+                audio_segment = AudioSegment(
+                    blob, sample_width=2, channels=1, frame_rate=16384
+                )
+                await post_blob(
+                    self.uid, count, audio_segment.export(format="mp4").read()
+                )
+                count += 1
+                await asyncio.sleep(0.01)
+
+    async def start(self):
+        self.uid = uuid.uuid4().hex
+        await start_recording(self.uid)
+        self.rtask = asyncio.create_task(self._record_and_transcribe())
+        self.is_recording = True
+
+    async def stop(self):
+        self.rtask.cancel()
+        await end_recording(self.uid)
+        self.is_recording = False
+
+    @on(Button.Pressed, "#recordButton")
+    async def toggle_recording(self) -> None:
+        """Event handler called when a button is pressed."""
+        if not self.is_recording:
+            await self.start()
+        else:
+            await self.stop()
+        btn = self.query_one("#recordButton")
+        btn.label = "Stop" if self.is_recording else "Record"
+        btn.variant = "error" if self.is_recording else "primary"
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets of a stopwatch."""
+        yield Button(label="Record", id="recordButton", variant="primary")
+        yield Input(
+            placeholder="Your prompt here",
+            id="promptInput",
+            value=self.text,
+        )
+
+
+class Salman(App):
+    """Salman the command line personal assistant."""
+
+    CSS_PATH = "salman.css"
+    BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Footer()
+        yield ScrollableContainer(PromptWidget(), id="container")
+
+    def action_toggle_dark(self) -> None:
+        """An action to toggle dark mode."""
+        self.dark = not self.dark
--- salman/salman/voice/recorder.py
+++ salman/salman/voice/recorder.py
@@ -1,33 +1,58 @@
+import asyncio
+
 import pyaudio
 
 from salman.logging import recorder_logger as logger
 
 
 class MicRecorder:
-    def __init__(self, rate=16384, chunk=16384):
+    def __init__(self, rate=16384, frames_per_buffer=16384):
         self.rate = rate
-        self.chunk = chunk
+        self.frames_per_buffer = frames_per_buffer
+        self.queue = asyncio.Queue()
 
-    def record(self, duration=5):
-        self.p = pyaudio.PyAudio()
-        device_name = self.p.get_default_input_device_info().get("name")
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, *args):
+        self.stream.stop_stream()
+        self.stream.close()
+        self.audio.terminate()
+
+    def start(self):
+        loop = asyncio.get_event_loop()
+        self.queue = asyncio.Queue()
+
+        def callback(in_data, frame_count, time_info, status):
+            loop.call_soon_threadsafe(self.queue.put_nowait, (in_data[:], status))
+            return in_data, pyaudio.paContinue
+
+        self.audio = pyaudio.PyAudio()
+        device_name = self.audio.get_default_input_device_info().get("name")
         logger.info(f'Recording on device "{device_name}"')
-        self.stream = self.p.open(
+        self.stream = self.audio.open(
             format=pyaudio.paInt16,
             channels=1,
             rate=self.rate,
             input=True,
-            frames_per_buffer=self.chunk,
+            frames_per_buffer=self.frames_per_buffer,
+            stream_callback=callback,
         )
         self.stream.start_stream()
+
+    def __aiter__(self):
+        return self.record()
+
+    async def record(self):
         try:
             while self.stream.is_active():
-                yield self.stream.read(duration * self.chunk)
+                indata, status = await self.queue.get()
+                yield indata, status
         except OSError:
             pass
-        logger.info(f'Stoped recording on device "{device_name}"')
 
-    def close(self):
+    def stop(self):
         self.stream.stop_stream()
         self.stream.close()
-        self.p.terminate()
+        self.audio.terminate()
